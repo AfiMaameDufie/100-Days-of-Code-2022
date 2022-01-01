d941d266031971e310138e098728b1a72bc75ee4
Abigail Afi Gbadago <afimaamedufie@gmail.com>
2022-01-01 18:30:28 +0000

    Creating a Hello World endpoint with Spring

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -15,7 +15,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/hello")
+	@GetMapping("/sayhello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
