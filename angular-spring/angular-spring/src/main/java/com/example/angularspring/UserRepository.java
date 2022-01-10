2bf1dfcff9677ea139c2cfae091225867db96a88
Abigail Afi Gbadago <afimaamedufie@gmail.com>
2022-01-10 20:14:57 +0000

    Create Angular-Spring project

---
+++ angular-spring/angular-spring/src/main/java/com/example/angularspring/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.angularspring;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User,Long>{}
