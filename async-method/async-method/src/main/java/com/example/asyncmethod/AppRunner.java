f23983be010b8a5110017bc632f824ec041fba52
Abigail Afi Gbadago <afimaamedufie@gmail.com>
2022-01-20 19:52:57 +0000

    Add Runner to show Lookup method being called async

---
+++ async-method/async-method/src/main/java/com/example/asyncmethod/AppRunner.java
@@ -0,0 +1,40 @@
+package com.example.asyncmethod;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+
+@Component
+public class AppRunner implements CommandLineRunner {
+    private static final Logger logger = LoggerFactory.getLogger(AppRunner.class);
+
+    private static GithubLookupService githubLookupService;
+
+    public AppRunner(GithubLookupService githubLookupService) {
+        this.githubLookupService = githubLookupService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        //Starting the clock
+        long Start = System.currentTimeMillis();
+
+        //Start the async lookups
+        CompletableFuture<User> page1 = githubLookupService.findUser("AfiMaameDufie");
+        CompletableFuture<User> page2 = githubLookupService.findUser("CloudFoundry");
+        CompletableFuture<User> page3 = githubLookupService.findUser("Spring-User");
+
+        //wait till the lookups are done
+        CompletableFuture.allOf(page1,page2,page3).join();
+
+        //Display results
+        logger.info("Time used: " + (System.currentTimeMillis() - Start));
+        logger.info("----" + page1.get());
+        logger.info("----" + page2.get());
+        logger.info("----" + page3.get());
+
+    }
+}
